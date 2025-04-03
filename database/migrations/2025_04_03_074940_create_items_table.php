e508cf224a6b65fd01a35776510b85c06b4a423d
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-04-03 13:48:37 +0530

    feat: add database design

---
+++ database/migrations/2025_04_03_074940_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Brand;
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Brand::class)->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(Category::class)->nullable()->constrained()->nullOnDelete();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('attachment');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
