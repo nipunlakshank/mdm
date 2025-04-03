15335dc791a834c39095d71c959a840b1b5506f8
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-04-03 16:41:43 +0530

    chore: cleanup views, add basic routes

---
+++ resources/views/livewire/brands.blade.php
@@ -0,0 +1,5 @@
+<div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+    <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Livewire\Brands;
+use App\Livewire\Categories;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -9,9 +11,11 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::get('brands', Brands::class)->name('brands');
+    Route::get('categories', Categories::class)->name('categories');
+});
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
